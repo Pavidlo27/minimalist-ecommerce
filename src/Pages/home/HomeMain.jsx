5c6ae67f5b78d77ce7e22578eb078580394a7584
Luka Shalamberidze <lkshalamberidze@gmail.com>
2024-10-09 19:10:48 +0400

    fix loading bug

--- src/Pages/home/HomeMain.jsx
+++ src/Pages/home/HomeMain.jsx
@@ -3,14 +3,17 @@ import img1 from '../../assets/images/livingroom.jpg'
 import img2 from '../../assets/images/img2.jpg'
 import img3 from '../../assets/images/img3.jpg'
 import img4 from '../../assets/images/kitchen.jpg'
-import LowResImg1 from '../../assets/images/livingroomLowRes.jpg'
+import LowResImg1 from '../../assets/images/lowResImg1.jpg'
 import LowResImg2 from '../../assets/images/img2LowRes.jpg'
+import LowResImg3 from '../../assets/images/img3LowRes.jpg'
+import LowResImg4 from '../../assets/images/kitchenLowRes.jpg'
 import { Link } from 'react-router-dom'
 
 const HomeMain = () => {
-  const [isHighResolutionLoaded, setIsHighResolutionLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const handleLoad = () => setIsLoaded(true)
   return (
-    <section className='mb-20' onLoad={() => setIsHighResolutionLoaded(true)}>
+    <section className='mb-20' onLoad={handleLoad}>
       <ul className='grid grid-cols-2 grid-rows-2 gap-2 md:gap-3 text-2xl text-white font-semibold
       lg:grid-cols-[2fr_1fr_1fr] lg:text-4xl'>
         <li className='relative lg:row-start-1 lg:row-end-3  overflow-hidden' data-aos="zoom-in">
@@ -18,9 +21,9 @@ const HomeMain = () => {
             <img
               src={LowResImg1}
               alt=""
-              className={`${isHighResolutionLoaded ? 'hidden' : 'blur-md'} w-full h-60 lg:h-[492px] object-cover brightness-75 hover:brightness-50 duration-500 cursor-pointer`} />
+              className={`absolute transition-opacity ${isLoaded ? 'opacity-0' : 'opacity-100 blur-md'} w-full h-60 lg:h-[492px] object-cover brightness-75 hover:brightness-50 duration-500 cursor-pointer`} />
             <img
-              className={`${isHighResolutionLoaded ? '' : 'hidden'} w-full h-60 lg:h-[492px] object-cover brightness-75 hover:brightness-50 duration-500 cursor-pointer`}
+              className={`${isLoaded ? '' : 'hidden'} w-full h-60 lg:h-[492px] object-cover brightness-75 hover:brightness-50 duration-500 cursor-pointer`}
               src={img1}
               alt="Live Comfortably"
             />
@@ -29,14 +32,14 @@ const HomeMain = () => {
             >Live Comfortably</span>
           </Link>
         </li>
-        <li className='relative lg:row-start-1 lg:row-end-3' data-aos="zoom-in">
+        <li className='relative lg:row-start-1 lg:row-end-3 overflow-hidden' data-aos="zoom-in">
           <Link to='categories?type=skin care'>
             <img
               src={LowResImg2}
               alt=""
-              className={`${isHighResolutionLoaded ? 'hidden' : 'blur-md'} w-full h-60 lg:h-[492px] object-cover brightness-75 hover:brightness-50 duration-500 cursor-pointer`} />
+              className={`${isLoaded ? 'hidden' : 'blur-md'} w-full h-60 lg:h-[492px] object-cover brightness-75 hover:brightness-50 duration-500 cursor-pointer`} />
             <img
-              className={`${isHighResolutionLoaded ? '' : 'hidden'} w-full h-60 lg:h-[492px] object-cover brightness-75 hover:brightness-50 duration-500 cursor-pointer`}
+              className={`${isLoaded ? '' : 'hidden'} w-full h-60 lg:h-[492px] object-cover brightness-75 hover:brightness-50 duration-500 cursor-pointer`}
               src={img2}
               alt="Live Comfortably"
             />
@@ -45,10 +48,14 @@ const HomeMain = () => {
             >Skincare</span>
           </Link>
         </li>
-        <li className='relative' data-aos="zoom-in">
+        <li className='relative overflow-hidden' data-aos="zoom-in">
           <Link to='categories?type=kitchen'>
             <img
-              className='w-full h-60 object-cover brightness-75 hover:brightness-50 duration-500 cursor-pointer'
+              src={LowResImg4}
+              className={`${isLoaded ? 'hidden' : 'blur-md'} w-full h-60 object-cover brightness-75 hover:brightness-50 duration-500 cursor-pointer`}
+            />
+            <img
+              className={`${!isLoaded && 'hidden'} w-full h-60 object-cover brightness-75 hover:brightness-50 duration-500 cursor-pointer`}
               src={img4}
               alt="Kitchen"
             />
@@ -57,11 +64,16 @@ const HomeMain = () => {
             >Kitchen</span>
           </Link>
         </li>
-        <li className='relative lg:col-start-3 lg:col-end-3 lg:overflow-hidden' data-aos="zoom-in">
+        <li className='relative lg:col-start-3 lg:col-end-3 overflow-hidden' data-aos="zoom-in">
           <Link to='categories?type=electronics'>
             <img
-              className='w-full h-60 object-cover brightness-75 
-              hover:brightness-50 duration-500 cursor-pointer'
+              src={LowResImg3}
+              className={`w-full h-60 object-cover brightness-75 
+              hover:brightness-50 duration-500 cursor-pointer ${isLoaded ? 'hidden' : 'blur-md'}`}
+            />
+            <img
+              className={`w-full h-60 object-cover brightness-75 
+              hover:brightness-50 duration-500 cursor-pointer ${!isLoaded && 'hidden'}`}
               src={img3}
               alt="Electronics"
             />
--- src/Pages/store/Products.jsx
+++ src/Pages/store/Products.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import loaderImg from '@/assets/images/BlackDot.png'
-const Products = ({ displayedProds, searchParams, searchFilter }) => {
-  const [isHighResolutionLoaded, setIsHighResolutionLoaded] = useState(false)
-  const productElements = displayedProds.map(prod => (
+
+const Product = ({ prod, searchParams }) => {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  return (
     <Link
       to={`/categories/${prod.id}`}
       key={prod.id}
@@ -12,21 +14,33 @@ const Products = ({ displayedProds, searchParams, searchFilter }) => {
       }}
       data-aos="zoom-in-up"
     >
-      <div className="border-2 border-gray-300 hover:border-black flex flex-col h-full">
-        <img src={loaderImg} alt="" className={`${isHighResolutionLoaded ? 'hidden' : ''}`} />
-        <img src={prod.firstImg} alt="product-image" className={`${isHighResolutionLoaded ? '' : 'hidden'}`} onLoad={() => setIsHighResolutionLoaded(true)} />
+      <div className="relative border-2 border-gray-300 hover:border-black flex flex-col h-full">
+        <img src={loaderImg} alt="loading" className={`absolute transition-opacity ${isLoaded ? 'opacity-0' : 'opacity-100'}`} />
+        <img
+          src={prod.firstImg}
+          alt="product-image"
+          className={`transition-opacity ${isLoaded ? 'opacity-100' : 'opacity-0'}`}
+          onLoad={() => setIsLoaded(true)}
+        />
         <div className="p-3 flex-grow flex flex-col">
           <h3 className="text-lg">{prod.name}</h3>
           <p className="text-lg font-semibold mt-auto">{prod.price}.00$</p>
         </div>
       </div>
     </Link>
+  )
+}
+
+const Products = ({ displayedProds, searchParams, searchFilter }) => {
+  const productElements = displayedProds.map(prod => (
+    <Product key={prod.id} prod={prod} searchParams={searchParams} />
   ))
+
   return (
     <div className='relative grid gap-3 grid-cols-2 lg:grid-cols-4'>
       {productElements.length > 0
         ? productElements
-        : <p className=' absolute left-0 right-0 text-center'>No products found for '{searchFilter}'</p>}
+        : <p className='absolute left-0 right-0 text-center'>No products found for '{searchFilter}'</p>}
     </div>
   )
 }
