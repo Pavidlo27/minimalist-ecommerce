5aed154328df8c64ca94f0a5418eb263fe63082d
Luka Shalamberidze <lkshalamberidze@gmail.com>
2024-10-12 19:13:41 +0400

    loader

--- src/Pages/store/Products.jsx
+++ src/Pages/store/Products.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Product = ({ prod, searchParams }) => {
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <Link
@@ -13,17 +13,17 @@ const Product = ({ prod, searchParams }) => {
       }}
       data-aos="zoom-in-up"
     >
-      <div className="relative border-2 border-gray-300 hover:border-black flex flex-col h-full">
+      <div className="relative border-2 border-gray-300 hover:border-black flex flex-col">
         <div
-          className={` h-64 flex items-center justify-center transition-opacity ${isLoaded ? 'hidden' : 'block'} duration-300`}
+          className={`aspect-square w-full bg-fuchsia-100 flex items-center justify-center transition-opacity ${isLoaded ? 'hidden' : 'block'} duration-300`}
         >
           <div className="loader"></div>
         </div>
         <img
           src={prod.firstImg}
           alt="product-image"
-          liading="lazy"
-          className={`transition-opacity ${isLoaded ? 'opacity-100' : 'opacity-0'}`}
+          loading="lazy"
+          className={`transition-opacity duration-300 ${isLoaded ? 'opacity-100' : 'opacity-0'}`} // Added duration for smoother transition
           onLoad={() => setIsLoaded(true)}
         />
         <div className="p-3 flex-grow flex flex-col">
@@ -32,21 +32,24 @@ const Product = ({ prod, searchParams }) => {
         </div>
       </div>
     </Link>
-  )
-}
+  );
+};
 
 const Products = ({ displayedProds, searchParams, searchFilter }) => {
+  // Use a more descriptive variable name
   const productElements = displayedProds.map(prod => (
     <Product key={prod.id} prod={prod} searchParams={searchParams} />
-  ))
+  ));
 
   return (
     <div className='relative grid gap-3 grid-cols-2 lg:grid-cols-4'>
-      {productElements.length > 0
-        ? productElements
-        : <p className='absolute left-0 right-0 text-center'>No products found for '{searchFilter}'</p>}
+      {productElements.length > 0 ? (
+        productElements
+      ) : (
+        <p className='absolute left-0 right-0 text-center'>No products found for '{searchFilter}'</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
